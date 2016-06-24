2d69d1944fc05509bff52eb085383f3f6411a96e
mat-eyo <maciej.matejko@wp.eu>
2016-06-24 14:27:49 +0200

    completed Team entity

--- task_manager/src/AppBundle/Controller/TeamController.php
+++ task_manager/src/AppBundle/Controller/TeamController.php
@@ -24,9 +24,10 @@ class TeamController extends Controller
     {
         $team = new Team();
         $form = $this->createFormBuilder($team)
-                ->add("name")->add("description")
+                ->add("name")
+                ->add("description")
                 ->add('users', 'entity', array('class' => 'AppBundle:User', 'choice_label' => 'username', 'multiple' => true))
-                ->add("submit", "submit")
+                ->add("add", "submit")
                 ->getForm();
         
         $form->handleRequest($request);
@@ -43,6 +44,107 @@ class TeamController extends Controller
             return $this->redirectToRoute("app_user_main");
         }
         
-        return ["form" => $form->createView()];
+        return $this->render("AppBundle:Team:new.html.twig", ["form" => $form->createView()]);
+    }
+    
+    /**
+    * @Route("/{id}/edit")
+    * @Template()
+    * @Security("has_role('ROLE_MANAGER')")
+    */
+    public function editAction(Request $request, $id)
+    {
+        $team = $this->getDoctrine()->getRepository("AppBundle:Team")->find($id);
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if(!$team){
+            throw $this->createNotFoundException("Team not found");
+        }
+        if($user->getTeam() == null || $team->getId() !== $user->getTeam()->getId()){
+            $this->redirectToRoute("app_user_error403");
+        }
+        
+        $form = $this->createFormBuilder($team)
+                ->add("name")
+                ->add("description")
+                ->add('users', 'entity', array('class' => 'AppBundle:User', 'choice_label' => 'username', 'multiple' => true))
+                ->add("edit", "submit")
+                ->getForm();
+        
+        $form->handleRequest($request);
+        
+        if($form->isValid()){
+            $users=$team->getUsers();
+            foreach($users as $user){
+                $user->setTeam($team);
+            }
+            $em = $this->getDoctrine()->getManager()->flush();
+            
+            return $this->redirectToRoute("app_user_main");
+        }
+        
+        return $this->render("AppBundle:Team:edit.html.twig", ["team" => $team, "form" => $form->createView()]);
+    }
+    
+    /**
+    * @Route("/{id}/delete")
+    * @Template()
+    * @Security("has_role('ROLE_MANAGER')")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $team = $this->getDoctrine()->getRepository("AppBundle:Team")->find($id);
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if(!$team){
+            throw $this->createNotFoundException("Team not found");
+        }
+        if($user->getTeam() == null || $team->getId() !== $user->getTeam()->getId()){
+            $this->redirectToRoute("app_user_error403");
+        }
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($team);
+        $em->flush();
+        
+        return $this->redirectToRoute("app_user_main");
+    }
+    
+    /**
+    * @Route("/{id}/show")
+    * @Template()
+    */
+    public function showAction($id)
+    {
+        $team = $this->getDoctrine()->getRepository("AppBundle:Team")->find($id);
+        
+        if(!$team){
+            throw $this->createNotFoundException("Team not found");
+        }
+        
+        return ["team" => $team];
+    }
+    
+    /**
+     * @Route("{id}/{userId}/remove")
+     */
+    public function removeAction($id, $userId)
+    {
+        $team = $this->getDoctrine()->getRepository("AppBundle:Team")->find($id);
+        
+        if(!$team){
+            throw $this->createNotFoundException("Team not found");
+        }
+        
+        $user = $this->getDoctrine()->getRepository("AppBundle:User")->find($userId);
+        
+        if(!$user){
+            throw $this->createNotFoundException("User not found");
+        }
+        
+        $em = $this->getDoctrine()->getManager();
+        $user->setTeam(null);
+        $team->removeUser($user);
+        $em->flush();
+        
+        return $this->redirectToRoute("app_team_edit", ["id" => $id]);
     }
 }
